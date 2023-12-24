2550e692344a5a899a3f328111950df99c3ea2d7
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2023-12-24 18:07:04 +0300

    Update digits_to_array.c

--- C/digits_to_array.c
+++ C/digits_to_array.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
-// prints the integer array side by side
-const void printArr(int arr[],int size) { 
-    for(int i=0;i<size;i++) {
-         printf("%d",arr[i]);       
-    }
-}
-
-// prints the integer array with a new line
-const void printArr_(int arr[],int size) { 
-    for(int i=0;i<size;i++) {
-         printf("%d\n",arr[i]);       
+// prints the integer array
+const void printArr(int arr[],int size,int mode) { 
+    switch (mode) {
+    case 1:                            //  Mode:1 Side by side with spaces
+        for(int i=0;i<size;i++)
+            printf("%d ",arr[i]);
+        break;
+    case 2:                            // Mode:2 With new line
+        for(int i=0;i<size;i++)
+            printf("%d\n",arr[i]);
+        break;
+    default:                          //  Default: Side by side
+        for(int i=0;i<size;i++)
+            printf("%d",arr[i]);
+        break;
     }
 }
 
@@ -47,6 +51,6 @@ int main() {
     int size = total_digits(number);     // calculates the size of the array we will create
     int arr[size];                       // Creates an array containing each digit of the number
     digits_to_array(number,arr,size);
-    printArr_(arr,size);                 // prints digits with new line
+    printArr(arr,size,2);                // prints digits with mode 2(with new line)
     
 }
