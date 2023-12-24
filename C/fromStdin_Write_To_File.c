4aac70168e959a7efc85d286c9e99cc74443a066
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2023-12-24 16:26:05 +0300

    Create fromStdin_Write_To_File.c
    Take input from stdin and write it to the file you pass as argument

---
+++ C/fromStdin_Write_To_File.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define BUFFER_S 255
+
+int main(int argc, char *argv[]) {
+        char *fileName = argv[1];
+        if(fileName == NULL) {
+                puts("Enter file name as argument");
+                exit(EXIT_FAILURE);
+        }
+        FILE *file;
+        file = fopen(fileName,"w");
+        if(file == NULL) {
+                puts("Error while opening file...");
+                exit(EXIT_FAILURE);
+        }
+        bool cont = true;
+        char buffer[BUFFER_S];
+        while(cont) {
+                printf(">");
+                fgets(buffer,BUFFER_S,stdin);
+                if(strcmp(buffer,"quit\n") == 0 || strcmp(buffer,"exit\n") == 0)
+                        exit(EXIT_SUCCESS);
+                fputs(buffer,file);
+        }
+        fclose(file);
+}
