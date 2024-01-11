11d5707ff346e9034963d6152fa530788c3b89f5
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2024-01-12 00:41:35 +0300

    Update macros.h

--- C/macros.h
+++ C/macros.h
@@ -4,15 +4,12 @@
 #define PI 3.14159
 #define DEG2RAD(deg) (deg * (PI/180))
 #define RAD2DEG(rad) (rad * (180/PI))
-#define array_size(arr) (*(&arr + 1)) - (arr)
-
-#define max(a,b) (a>b ? a : b)
-#define min(a,b) (a>b ? b  : a)
 
 #define d_sin(degree) (sin(DEG2RAD(degree)))
 #define d_cos(degree) (cos(DEG2RAD(degree)))
 
+#define max(a,b) (a>b ? a : b)
+#define min(a,b) (a>b ? b : a)
 
-int main() {
-  
-}
+#define array_size(arr) (*(&arr + 1)) - (arr)
+#define PRINTARRAY(arr) for(int i=0;i<array_size(arr);i++) printf("%d ",arr[i]);
