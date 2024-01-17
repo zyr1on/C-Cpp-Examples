6d14a6845e63eb4049135034e1fb8e14ca491cbc
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2024-01-18 00:26:51 +0300

    Update complex.c

--- C/complex.c
+++ C/complex.c
@@ -9,30 +9,30 @@ typedef struct {
 
 complex operation(char type[],complex c1,complex c2) { 
     complex result = {0,0};
-	if(strcmp("add",type) == 0 ||strcmp("sum",type) == 0) {
+	if(strcmp("+",type) == 0 ||strcmp("sum",type) == 0) {
 		result.real = c1.real + c2.real;
 		result.im = c1.im + c2.im;
 	}
-	else if(strcmp("sub",type) == 0 ||strcmp("substract",type) == 0) {
+	else if(strcmp("-",type) == 0 ||strcmp("sub",type) == 0) {
 		result.real = c1.real - c2.real;
 		result.im = c1.im - c2.im;
 	}
-	else if(strcmp("div",type) == 0 ||strcmp("divide",type) == 0) {
+	else if(strcmp("/",type) == 0 ||strcmp("divide",type) == 0) {
 		result.real = (c1.real*c2.real + c1.im *c2.im) / (c2.real*c2.real + c2.im * c2.im);
 		result.im = (c1.im * c2.real - c1.real*c2.im) / (c2.real*c2.real + c2.im * c2.im);
 	}
-	else if(strcmp("multi",type) == 0 ||strcmp("multiply",type) == 0) {
+	else if(strcmp("multiply",type) == 0 ||strcmp("*",type) == 0) {
 		result.real = c1.real*c2.real - c1.im *c2.im;
 		result.im = c1.real*c2.im + c1.im * c2.real;
 	}
 	else
-		printf("Operations is sum/sub/div/multi\n");
+		printf("Operations are sum,+ | sub,- | div,/ | multi,* \n");
 	return result;
 }
 main() {
 	complex c1 = {3,-1};
 	complex c2 = {2,-2};
-	complex example = operation("div",c1,c2);
+	complex example = operation("*",c1,c2);
 	printf("%.1fi + %.1fj\n",example.real,example.im);
 	return 0;
 }
