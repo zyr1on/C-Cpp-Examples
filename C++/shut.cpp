72e77bb8548739dcbccc9eb345a69143c16d4afd
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2024-01-29 03:27:00 +0300

    Update shut.cpp

--- C++/shut.cpp
+++ C++/shut.cpp
@@ -2,9 +2,7 @@
 g++ shut.cpp -o shut.exe -static-libstdc++ -static -static-libgcc
 'if output file is shutdown.exe it won't work, idk why :)'
 */
-
 #include <iostream>
-
 using std::string;
 using std::cout;
 using std::cin;
@@ -13,20 +11,19 @@ using std::to_string;
 void shut_min(int min) {
     cout << min << " Minustes to shutdown\n";
     min = min * 60;
-    string min_ = to_string(min);
-    string cmd = "shutdown -s -f -t " + min_;
+    string cmd = "shutdown -s -f -t " + to_string(min);
     system(cmd.c_str());
 }
 void shut_sec(int sec) {
     cout << sec << " Seconds to shutdown\n";
-    string sec_ = to_string(sec);
-    string cmd = "shutdown -s -f -t " + sec_;
+    string cmd = "shutdown -s -f -t " + to_string(sec);
     system(cmd.c_str());
 }
 void abort_shut() {
     cout << "Aborted\n";
     system("shutdown -a");
 }
+
 int main() {
     int time,pref;
     cout << "1-Mins\n2-Secs\n3-Abort scheduled shutdown\n";
@@ -42,7 +39,7 @@ int main() {
         break;
     case 3:
         abort_shut();
-        break;    
+        break;
     default:
         cout << "Enter number between 1-3\n";
         break;
