d7231cd483b0c7b76dd6f51e63255cd5cc0b9850
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2024-01-31 23:11:00 +0300

    Update main.cpp

--- C++/shutdownApp/main.cpp
+++ C++/shutdownApp/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #if __linux__
-        #include "shut_linux.hpp"
-    #elif _WIN32
-        #include "shut_windows.hpp"
-    #endif
+    #include "shut_linux.hpp"
+#elif _WIN32
+    #include "shut_windows.hpp"
+#endif
 int main() {
     run();
     return 0;
