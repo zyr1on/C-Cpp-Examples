a524fb4941221fb2dd63c4105dcdad67874bc918
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2024-02-07 03:46:37 +0300

    Create MoreSimple.cpp

---
+++ C++/UltraSimpleTextEditor/MoreSimple.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+
+using std::cout;
+using std::cin;
+using std::fstream;
+using std::string;
+
+int main(int argc, char *argv[])
+{
+    char *fileName = argv[1];
+    if(fileName == NULL) {
+        puts("Enter file name as argument | ./app <filename>\n");
+        exit(EXIT_FAILURE);
+    }
+    fstream file (fileName,std::ios::in | std::ios::out | std::ios::app);
+    string line;
+    int count = 1;
+    bool cont = true;
+    if(file.is_open()) {
+        cout << "Type 'quit' to exit the program\n";
+        while(getline(file,line)) {
+            cout << count << " " << line << "\n";
+            count++;
+        }
+        file.clear();
+        while(cont) {
+            cout << count << " ";
+            getline(cin,line);
+            if(line == "quit") {
+                file.close();
+                cont = false;
+            }
+            else {
+                file << line << "\n";
+                count++;
+            }
+        }
+    }
+    return 0;
+}
