96c127f0883636a6fea7f2f558f117c2eb05fac6
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2024-02-08 16:23:38 +0300

    Create editor.c

---
+++ C/UltraSimpleTextEditor/editor.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define BUFFER_SIZE 1024
+
+void readFile(char *fileName) {
+    int counter = 1;
+    FILE *file;
+    file = fopen(fileName,"r");
+    char string[BUFFER_SIZE];
+    while(fgets(string,BUFFER_SIZE,file)) {
+        printf("%d %s",counter,string);
+        counter++;
+    }
+    fclose(file);
+}
+
+void writeFile(char *fileName)  {
+    FILE *file;
+    file = fopen(fileName,"w");
+    char string[BUFFER_SIZE];
+    bool cont = true;
+    int counter = 1;
+    while(cont) {
+        printf("%d ",counter);
+        fgets(string,BUFFER_SIZE,stdin);
+        if(strcmp(string,":q\n") == 0) {
+            exit(EXIT_SUCCESS);
+        }
+        fputs(string,file);
+        counter++;
+    }
+    fclose(file);
+}
+void editFile(char *fileName) {
+    int counter = 1;
+    bool cont = true;
+    FILE *file;
+    file = fopen(fileName,"r+");
+    char string[BUFFER_SIZE];
+    while(fgets(string,BUFFER_SIZE,file)) {
+        printf("%d %s",counter,string);
+        counter++;
+    }
+    while(cont) {
+        printf("%d ",counter);
+        fgets(string,BUFFER_SIZE,stdin);
+        if(strcmp(string,":q\n") == 0) {
+            exit(EXIT_SUCCESS);
+        }
+        fputs(string,file);
+        counter++;
+    }
+    fclose(file);
+}
+
+int fileExists(char *fileName)
+{
+    FILE *file;
+    if ((file = fopen(fileName, "r")))
+    {
+        fclose(file);
+        return 1;
+    }
+    return 0;
+}
+
+int main(int argc, char const *argv[]) {
+    char *fileName = argv[1];
+    if(fileName == NULL) {
+        puts("Enter file name as argument | ./app <filename>\n");
+        exit(EXIT_FAILURE);
+    }
+    if(fileExists(fileName)) {
+        puts("File exists. Editing file. Type ':q' to exit");
+        editFile(fileName);
+    }
+    else {
+        puts("File not exists. Creating and Writing file. Type ':q' to exit.");
+        writeFile(fileName);
+    }
+    return 0;
+}
