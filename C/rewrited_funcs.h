dd71ad37019563c2541449f2046696e4a60f57f8
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2024-02-27 09:54:18 +0300

    Update rewrited_funcs.h

--- C/rewrited_funcs.h
+++ C/rewrited_funcs.h
@@ -25,3 +25,7 @@ void readInt(int *val) {
 void readChar(char *c) {
 	scanf("%c",c);
 }
+void readStr(char * str) {
+	fgets(str,sizeof(str),stdin);
+	// If you declare the string as 'char *' it probably won't work (cause:size of string)
+}
