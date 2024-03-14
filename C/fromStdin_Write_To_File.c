cb8590753484bb6e6b351080cae38b588b4348ab
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2024-03-14 21:08:35 +0300

    Update fromStdin_Write_To_File.c

--- C/fromStdin_Write_To_File.c
+++ C/fromStdin_Write_To_File.c
@@ -1,35 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
-
 #define BUFFER_S 1024
-
 int main(int argc, char *argv[]) {
-        char *fileName = argv[1];
-        if(fileName == NULL) {
-                puts("Enter file name as argument");
-                exit(EXIT_FAILURE);
-        }
-        FILE *file;
-        file = fopen(fileName,"w");
-        if(file == NULL) {
-                puts("Error while opening file...");
-                exit(EXIT_FAILURE);
-        }
-        bool cont = true;
-        char buffer[BUFFER_S];
-		puts("type :q or :quit to exit.");
-		while(cont) {
-                printf("> ");
-                fgets(buffer,BUFFER_S,stdin);
-                if(strcmp(buffer,":quit\n") == 0 || strcmp(buffer,":q\n") == 0) {
-						fclose(file);
-						break;
-						/* exit(EXIT_SUCCESS); */
-				}
-				fputs(buffer,file);
-        }
-        /* fclose(file); */
+    char *fileName = argv[1];
+    if(fileName == NULL) {
+        fprintf(stderr,"%s\n","Enter file name as argument");
+        exit(EXIT_FAILURE);
+    }
+    FILE *file;
+    file = fopen(fileName,"w");
+    if(file == NULL) {
+        fprintf(stderr,"%s\n","Error while opening file...");
+        exit(EXIT_FAILURE);
+    }
+    char buffer[BUFFER_S];
+	puts("type :q or :quit to exit.");
+	while(1) {
+        printf("> ");
+        fgets(buffer,BUFFER_S,stdin);
+        if(strcmp(buffer,":quit\n") == 0 || strcmp(buffer,":q\n") == 0)
+	        break;
+	    fputs(buffer,file);
+	    fflush(file); //clears buffer;
+    }
+    fclose(file);
+    puts("File Closed");
 }
-
