6e9e26f2ca52c64919c3595294f095f208475c9f
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2024-03-14 21:17:26 +0300

    Update sameButMoreSimple.c

--- C/UltraSimpleTextEditor/sameButMoreSimple.c
+++ C/UltraSimpleTextEditor/sameButMoreSimple.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-
+#include <stdlib.h>
 #define BUFFER_SIZE 1024
 
-int main(int argc, char const *argv[]) {
-    char *fileName = argv[1];
+int main(int argc, char *argv[]) {
+    char* fileName = argv[1];
     if(fileName == NULL) {
-        puts("Enter file name as argument | ./app <filename>\n");
+        fprintf(stderr,"%s\n","Enter file name as argument | ./app <filename>");
         exit(EXIT_FAILURE);
     }
     FILE *file;
     file = fopen(fileName,"a+");
-    char string[BUFFER_SIZE];
+    if(file == NULL) {
+        fprintf(stderr,"%s\n","Error while opening file...");
+        exit(EXIT_FAILURE);
+    }
+    char line[BUFFER_SIZE];
     int counter = 1;
     puts("Type ':q' to exit the program");
-    while(fgets(string,BUFFER_SIZE,file)) {
-        printf("%d %s",counter,string);
-        counter++;
-    }
+    while(fgets(line,BUFFER_SIZE,file))
+        printf("%d %s",counter++,line);
+    clearerr(file);
     while(1) {
         printf("%d ",counter);
-        fgets(string,BUFFER_SIZE,stdin);
-        if(strcmp(string,":q\n") == 0)
+        fgets(line,BUFFER_SIZE,stdin);
+        if(strcmp(line,":q\n") == 0 || strcmp(line,":quit\n") == 0 )
             break;
-        fputs(string,file);
+        fputs(line,file);
+        fflush(file);
         counter++;
     }
     fclose(file);
