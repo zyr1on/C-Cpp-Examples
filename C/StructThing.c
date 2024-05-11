363e383189b23ea681d2d3a7af7a5d6dff0a2dbc
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2024-05-11 21:14:02 +0300

    Update StructThing.c

--- C/StructThing.c
+++ C/StructThing.c
@@ -1,27 +1,23 @@
-	#include <stdio.h>
-	#include <stdlib.h>
-	#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-	typedef struct {
-		char *chars; //unknown length of string, need to allocate that;
-		int(*length)(struct String* string);
-	}String;
+typedef struct {
+	char* data; //unknown length of string, need to allocate that;
+	int length;
+}String;
 
-	int length(String* string) {
-		return strlen(string->chars);
-	}
-	String *initialize(int n) {
-		String* str = malloc(sizeof(str));
-		str->chars = malloc(sizeof(char) * n);
-		str->length = length;
-		str->chars[0] = '\0';
-		return str;
-	}
-	int main(int argc, char const *argv[]) {
-		String* str = initialize(10);
-		strcpy(str->chars,"hello");
-		printf("%d\n",str->length(str));
-		free(str);
-		return 0;
-	}
+String* init(char* source) {
+	String* str = malloc(sizeof(String));
+	str->length = strlen(source);
+	str->data = malloc(sizeof(char) * str->length);
+	strcpy(str->data,source);
+	return str;
+}
+typedef String* string;
+int main(int argc, char const *argv[]) {
+	string str = init("hello world");
+	printf("%s\n",str->data);
+	free(str);
+}
 
