add46c4668556ce4e559a1d2e7536c73bafbbd1a
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2024-08-06 18:10:33 +0300

    Update math.hpp

--- C++/math.hpp
+++ C++/math.hpp
@@ -1,20 +1,50 @@
 #include <iostream>
 #include <cmath>
 #include <random>
+
 namespace Math
 {
     long double randomReal(long double min=0,long double max=1) {
         static std::random_device rseed;
         static std::mt19937 rng(rseed());
-        std::uniform_real_distribution<double> dist(min,max);
+        std::uniform_real_distribution<long double> dist(min,max);
         return dist(rng);
     }
     long int randomInt(long int min=0,long int max=1) {
         static std::random_device rseed;
         static std::mt19937 rng(rseed());
-        std::uniform_int_distribution<int> dist(min,max);
+        std::uniform_int_distribution<long int> dist(min,max);
         return dist(rng);
     }
+
+    struct Vector2{
+        long double x;
+        long double y;
+        Vector2() : x(0),y(0) {}
+        Vector2(long double x,long double y) : x(x),y(y) {}
+        Vector2 operator+(const Vector2& other) {
+            return { this->x + other.x , this->y + other.y };
+        }
+        Vector2 operator-(const Vector2& other) {
+            return { this->x - other.x , this->y - other.y };
+        }
+        void operator=(const Vector2& other) {
+            this->x = other.x;
+            this->y = other.y;
+        }
+        bool operator==(const Vector2& other) {
+            if(this->x == other.x && this->y == other.y)return true;
+            return false;
+        }
+        friend std::ostream& operator<<(std::ostream& os, Math::Vector2& other) {
+            os << "("<<other.x<<","<<other.y<<")";  
+            return os;
+        }
+        long double magnitude() 
+        {
+            return sqrt(this->x*this->x + this->y*this->y);
+        }
+    };
     
     struct complex 
     {
@@ -36,6 +66,7 @@ namespace Math
             long double denominator = other.real * other.real + other.im * other.im;
             if (denominator == 0)
                 throw std::runtime_error("Division by zero");
+    
             return {(this->real*other.real + this->im*other.im) / (other.real*other.real + other.im *other.im), (this->im*other.real - this->real*other.im) / (other.real*other.real + other.im *other.im)};
         }
         bool operator==(const complex& other) {
@@ -57,10 +88,13 @@ namespace Math
             return atan2(this->im,this->real);
         }
         friend std::ostream& operator<<(std::ostream& os, Math::complex& other) {
-            if(other.im < 0 && other.real < 0 || other.im < 0 && other.real>= 0) os << other.real<<other.im<<"i";
+            if(other.im < 0 && other.real < 0 || other.im < 0 && other.real>= 0)
+                os << other.real<<other.im<<"i";
             else os << other.real <<"+"<<other.im<<"i";    
             return os;
         }
         // make polar form complex struct
     };
 } // namespace Math
+
+
