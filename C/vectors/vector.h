03923888f93d9912dc87ce62b2f73cdfd7346626
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2024-08-06 17:17:30 +0300

    Update vector.h

--- C/vectors/vector.h
+++ C/vectors/vector.h
@@ -1,49 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <time.h>
 
-// I WILL OPTIMIZE IT asap!!!
+#define INITIAL_CAP 5
+int compare(const void* a, const void* b) {
+   return (*(int*)a - *(int*)b);
+} // for qsort!
+
 
 typedef struct {
     int* data;
     size_t size;
+    size_t capacity;
 } vector;
 
 // vector v;
 // vector_init(&v);
+// vector_destroy(&v);
 
-int vector_init(vector*v) { 
-    v->data = (int*)malloc(sizeof(int)); // 4byte;
-    if(v->data == NULL) {
-        perror("vector_init: Memory allocation error");
-        return -1;
+int v_initialized = 0;
+void vector_init(vector*v) {
+    if(v_initialized == 1) {
+        fprintf(stderr, "vector_init: Vector is already initialized\n");
+        return;
     }
-    v->size = 0;
-    return 0;
+    else {
+        v->data = (int*)malloc(sizeof(int)*INITIAL_CAP); // 4byte;
+        if(v->data == NULL) {
+            perror("vector_init: Memory allocation error\n");
+            return;
+        }
+        v->size = 0;
+        v->capacity = INITIAL_CAP;
+        v_initialized = 1;
+        return;
+    }  
 }
+
 int vector_push_back(vector*v, int element) { //vector_push_back(&v,element);
-    if (v == NULL) {
+    if (v->data == NULL) {
         fprintf(stderr, "vector_push_back: Vector is NULL\n");
         return -1;
     }
-    if(v->size == 0) {
-        v->data[v->size] = element;
-        v->size++; // 1->stack
-    }
-    else {
-        v->data = (int*) realloc(v->data,(v->size + 1) * sizeof(int)); //resize
-        if(v->data == NULL) {
-            perror("vector_push_back: Memory reallocation error:");
+    if(v->size < v->capacity) {
+        v->data[v->size++] = element;
+    }else {
+        v->capacity *=2;
+        int* temp = (int*)malloc(sizeof(int) * v->capacity);
+        if(temp == NULL) {
+            perror("vector_push_back: Memory reallocation error");
             return -1;
         }
-        v->data[v->size] = element;
-        v->size++; // 1+->stack
+        for(int i=0;i<v->size;i++) {
+            temp[i] = v->data[i];
+        }
+        free(v->data);
+        v->data = temp;
+        v->data[v->size++] = element;
     }
     return 0;
 }
-
+int vector_push_front(vector* v,int element) {
+    if (v->data == NULL) {
+        fprintf(stderr, "vector_push_front: Vector is NULL\n");
+        return -1;
+    }
+    if(v->size < v->capacity) {
+        for(int i=v->size;i>0;i--)
+            v->data[i] = v->data[i-1];
+        v->data[0] = element;
+        v->size++;
+    }
+    else {
+        v->capacity *=2;
+        int* temp = (int*)malloc(sizeof(int) * v->capacity);
+        if(temp == NULL) {
+            perror("vector_push_back: Memory reallocation error");
+            return -1;
+        }
+        for(int i=0;i<v->size;i++)
+            temp[i+1] = v->data[i];
+        free(v->data);
+        v->data = temp;
+        v->data[0] = element;
+    }
+    
+}
 int vector_delete(vector* v,int element) { //vector_delete(&v,element);
-    if (v == NULL || v->size == 0) {
+    if (v->data == NULL || v->size == 0) {
         fprintf(stderr, "vector_delete: Vector is empty or NULL\n");
         return -1;
     }
@@ -52,21 +96,17 @@ int vector_delete(vector* v,int element) { //vector_delete(&v,element);
         if(v->data[i] == element)
             index = i;
     if(index != -1) {
-        vector* tempVec = (vector*) malloc(sizeof(vector));
-        if(tempVec == NULL) perror("vector_delete: Memory reallocation error");
-        tempVec->data = (int*) malloc(sizeof(int)*(v->size -1));
-        tempVec->size = v->size -1;
+        int* tempData = (int*)malloc(sizeof(int)*v->capacity);
+        if(tempData == NULL) perror("vector_delete: Memory reallocation error");
         for(int i=0;i<index;i++) {
-            tempVec->data[i] = v->data[i];
+            tempData[i] = v->data[i];
         }
-        for(int i=index;i<tempVec->size;i++) {
-            tempVec->data[i] = v->data[i+1];
+        for(int i=index;i<v->size;i++) {
+            tempData[i] = v->data[i+1];
         }
-        v->data = (int*) realloc(v->data,sizeof(int)*(tempVec->size));
-        if(v->data == NULL) perror("vector_delete: Memory reallocation error");
+        free(v->data);
+        v->data = tempData;
         v->size--;
-        memcpy(v->data,tempVec->data,sizeof(int) * (tempVec->size));
-        free(tempVec->data);
         return 0;
     }
     else {
@@ -75,63 +115,48 @@ int vector_delete(vector* v,int element) { //vector_delete(&v,element);
     }
 }
 int vector_sort(vector* v) { //selection sort;
-    if (v == NULL || v->size == 0) {
+    if (v->data == NULL || v->size == 0) {
         fprintf(stderr, "vector_sort: Vector is empty or NULL\n");
         return -1;
     }
-    int min_index;
-    for(int i=0;i<v->size -1;i++) {
-        min_index = i;
-        for(int j=i+1;j<v->size;j++)
-            if(v->data[j] < v->data[min_index]) min_index = j;
-        if(min_index != i) {
-            int temp = v->data[min_index];
-            v->data[min_index] = v->data[i];
-            v->data[i] = temp;
-        }
-    }
+    qsort(v->data,v->size,sizeof(int),compare);
+    return 0;
+    
 }
 int vector_pop(vector* v) {
-    if (v == NULL || v->size == 0) {
+    if (v->data == NULL || v->size == 0) {
         fprintf(stderr, "vector_pop: Vector is empty or NULL\n");
         return -1;
     }
     if(v->size > 0) {
         v->size--;
-        v->data = (int*) realloc(v->data,sizeof(int)* v->size);
-        if(v->data == NULL) {
-            perror("vector_pop: Memory reallocation error");
-            return -1;
-        }
-        return 0;
+        return v->data[v->size+1];
     }
 }
 
 int vector_multiply(vector* v) {
-     if (v == NULL || v->size == 0) {
+     if (v->data == NULL || v->size == 0) {
         fprintf(stderr, "vector_multiply: Vector is empty or NULL\n");
         return -1;
     }
     int result = 1;
-    for(int i=0;i<v->size;i++) {
+    for(int i=0;i<v->size;i++)
         result *= v->data[i];
-    }
     return result;
 }
 int vector_sum(vector* v) {
-     if (v == NULL || v->size == 0) {
+     if (v->data == NULL || v->size == 0) {
         fprintf(stderr, "vector_sum: Vector is empty or NULL\n");
         return -1;
     }
     int sum = 0;
-    for(int i=0;i<v->size;i++) {
+    for(int i=0;i<v->size;i++)
         sum += v->data[i];
-    }
     return sum;
 }
 
 float vector_avg(vector* v) {
-    if (v == NULL || v->size == 0) {
+    if (v->data == NULL || v->size == 0) {
         fprintf(stderr, "vector_avg: Vector is empty or NULL\n");
         return -1;
     }
@@ -143,7 +168,7 @@ float vector_avg(vector* v) {
 }
 
 int vector_get_last_element(vector* v) {
-    if (v == NULL || v->size == 0) {
+    if (v->data == NULL || v->size == 0) {
         fprintf(stderr, "vector_get_last_element: Vector is empty or NULL\n");
         return -1;
     }
@@ -156,48 +181,57 @@ int vector_get_last_element(vector* v) {
 }
 
 int vector_max(vector* v) {
-    if (v == NULL || v->size == 0) {
+    if (v->data == NULL || v->size == 0) {
         fprintf(stderr, "Error: Vector is empty or NULL\n");
         return -1;
     }
 
     int max = v->data[0];
-    for(int i=1;i<v->size;i++) {
-        if(v->data[i] > max) {
+    for(int i=1;i<v->size;i++)
+        if(v->data[i] > max) 
             max = v->data[i];
-        }
-    }
     return max;
 }
 int vector_min(vector* v) {
-    if (v == NULL || v->size == 0) {
+    if (v->data == NULL || v->size == 0) {
         fprintf(stderr, "Error: Vector is empty or NULL\n");
         return -1;
     }
     int min = v->data[0];
-    for(int i=1;i<v->size;i++) {
-        if(v->data[i] < min) {
+    for(int i=1;i<v->size;i++)
+        if(v->data[i] < min)
             min = v->data[i];
-        }
-    }
     return min;
 }
 int vector_size(vector *v) {
 	return v->size;
 }
-int print_vector(vector*v) {
-	if (v == NULL || v->size == 0) {
+int vector_print(vector*v) {
+	if (v->data == NULL || v->size == 0) {
         fprintf(stderr, "Error: Vector is empty or NULL\n");
         return -1;
     }
-    for(int i=0;i<v->size;i++) printf("%d ",v->data[i]);
+    printf("[");
+    for(int i=0;i<v->size-1;i++) printf("%d,",v->data[i]);
+    printf("%d]",v->data[v->size-1]);
     puts("");
     return 0;
 }
 
-int destroy_vector(vector*v){
-    if (v == NULL) {
-        fprintf(stderr, "destroy_vector: Vector is empty or NULL\n");
+
+void vector_shuffle(vector* vec) {
+    srand(time(0));
+    for(int i=0;i<vec->size;i++) {
+        int randNum = rand() % vec->size;
+        int temp = vec->data[i];
+        vec->data[i] = vec->data[randNum];
+        vec->data[randNum] = temp;
+    }
+}
+
+int vector_destroy(vector*v){
+    if (v->data == NULL) {
+        fprintf(stderr, "vector_destroy: Vector is NULL maybe already destroyed\n");
         return -1;
     }
     free(v->data);
