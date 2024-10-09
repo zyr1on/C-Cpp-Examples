1f948a2af0ecd60bd5a2eb90aabf4d1543733445
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2024-10-09 16:10:11 +0300

    Update basic_ECS.cpp

--- C++/basic_ECS.cpp
+++ C++/basic_ECS.cpp
@@ -1,62 +1,56 @@
 #include <iostream>
 #include <memory>
 #include <vector>
-#include <typeinfo>
 
-struct Component {
-    virtual ~Component() {} 
+struct Component 
+{
+    virtual ~Component() {}
     virtual void update() = 0;
 };
-
-struct Position : public Component 
+struct Position : Component
 {
-    int x, y, z;
-    Position(int x, int y, int z) : x(x), y(y), z(z) {}
-    void update() override {
-        std::cout << "Position updated: " << x << ", " << y << ", " << z << "\n";
+    int x,y,z;
+    Position(int x,int y,int z) : x(x),y(y),z(z) {}
+    void update() override 
+    {
+        std::cout<< "Position("<< x <<","<<y<<","<<z<<")";
+    }
+};
+struct Color : Component
+{
+    int r,g,b;
+    Color(int r,int g,int b) : r(r),g(g),b(b) {}
+    void update() override 
+    {
+        std::cout<< "Color("<< r <<","<<g<<","<<b<<")";
     }
 };
 
-struct Color : public Component 
-{
-    int r, g, b;
-    Color(int r, int g, int b) : r(r), g(g), b(b) {}
-    void update() override {
-        std::cout << "Color updated: " << r << ", " << g << ", " << b << "\n";
-    }
-};
-
-class Entity 
+class Entitiy 
 {
 private:
     std::vector<std::unique_ptr<Component>> components;
 public:
-    template <typename T, typename... Args>
+    template<typename T,typename... Args>
     void addComponent(Args&&... args) 
     {
         components.emplace_back(std::make_unique<T>(std::forward<Args>(args)...));
     }
-    template <typename T>
-    T* getComponent() {
-        for (const auto& component : components)
-            if (T* castedComponent = dynamic_cast<T*>(component.get())) 
-                return castedComponent;
-        return nullptr;
-    }
-    void updateComponents() 
+    template<typename T>
+    T* getComponent() 
     {
-        for (const auto& component : components)
-            component->update();
+        for(const auto& component : components)
+            if(T* casted_component = dynamic_cast<T*>(component.get()))
+                return casted_component;
+        return nullptr;
     }
 };
 
-int main() {
-    Entity player;
-    player.addComponent<Position>(0, 0, 5);
-    player.addComponent<Color>(255, 125, 200); 
-
-    player.updateComponents();
-    std::cout<< player.getComponent<Color>()->g<<"\n";
+int main() 
+{
+    Entitiy player;
+    player.addComponent<Position>(0,15,25);
+    std::cout<<player.getComponent<Position>()->y<<"\n";
 
     return 0;
 }
