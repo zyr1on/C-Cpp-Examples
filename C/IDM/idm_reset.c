9d9dbf36a2456a6f3f0289a9cf758f4f988712bd
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2024-12-02 19:51:18 +0300

    Update idm_reset.c

--- C/IDM/idm_reset.c
+++ C/IDM/idm_reset.c
@@ -1,8 +1,27 @@
 #include <windows.h>
 #include <stdio.h>
 #include <string.h>
-
+#include<tlhelp32.h>
 #define MAX_KEY_LENGTH 255
+
+DWORD GetProcessID(const char *processName) {
+    HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (hSnapshot == INVALID_HANDLE_VALUE)
+        return 0;
+    PROCESSENTRY32 pe32;
+    pe32.dwSize = sizeof(PROCESSENTRY32);
+    if (Process32First(hSnapshot, &pe32)) {
+        do {
+            if (strcmp(pe32.szExeFile, processName) == 0) {
+                DWORD processID = pe32.th32ProcessID;
+                CloseHandle(hSnapshot);
+                return processID;
+            }
+        } while (Process32Next(hSnapshot, &pe32));
+    }
+    CloseHandle(hSnapshot);
+    return 0;
+}
 
 char* EnumerateKeys(HKEY hKeyRoot) {
     HKEY hKey;
@@ -33,14 +52,31 @@ char* EnumerateKeys(HKEY hKeyRoot) {
 int main() {
     char* key = EnumerateKeys((HKEY)HKEY_USERS);
     char keyPath[255];
+    const char* targetProcess = "IDMan.exe";
 	if (key != NULL) {
     	sprintf(keyPath,"%s\\WOW6432Node\\CLSID\\{07999AC3-058B-40BF-984F-69EB1E554CA7}",key);
 	    free(key);
     }
     printf("%s\n",keyPath);
-    
-    HKEY hKey;
+    DWORD processID = GetProcessID(targetProcess);
     LONG result = RegOpenKeyEx(HKEY_USERS, keyPath, 0, KEY_READ, &hKey);
+    HKEY hKey;
+    if(processID == 0) 
+    {
+        printf("'%s' was not found or not working.\n",targetProcess);
+    }
+    HANDLE hProcess = OpenProcess(PROCESS_TERMINATE,FALSE,processID);
+    if (hProcess == NULL) {
+        printf("Failed to open the process. Error code: %lu\n", GetLastError());
+    }
+    if(TerminateProcess(hProcess,0)) 
+    {
+        MessageBox(NULL, "IDMan.exe process terminated.", "Success", MB_ICONINFORMATION);
+    }
+    else {
+        printf("'%s' Failed to terminate the process.",targetProcess);
+    }
+    CloseHandle(hProcess);
     if (result == ERROR_SUCCESS) {
         int result_ = MessageBox(NULL, "Key Found! Are you want to delete key", keyPath, MB_YESNO | MB_ICONEXCLAMATION);
         if(result_ == IDYES) {
